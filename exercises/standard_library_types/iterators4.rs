8cdf650bf90b71fa746c721f89985c8cb8cad29e
tsukiy0 <tsukiy0@users.noreply.github.com>
2022-07-23 06:32:15 +0000

    feat: complete iterators

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,9 +1,9 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
+    return (1..=num).fold(1, |acc, n| acc * n);
+
     // Complete this function to return the factorial of num
     // Do not use:
     // - return
